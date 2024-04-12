6023e9c31b7e37698cb66180c1ae95b49a251532
William Santana <euwilliamsantana@gmail.com>
2024-04-12 13:26:05 -0300

    feat: Product Container CSS

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
+import { HomeContainer, Product } from '@/styles/pages/home'
+import camisa1 from '@/assets/tshirt1.svg'
+import camisa2 from '@/assets/tshirt1.svg'
+
+import Image from 'next/image'
+
 export default function Home() {
-  return <h1>Hello Word</h1>
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camisa1} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camisa2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
 }
